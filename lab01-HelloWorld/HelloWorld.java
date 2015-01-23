555aa6c9d904f68dc2c9e39a21485262096ac3fc
tihm <tjb318@lehigh.edu>
2015-01-23 19:35:23 +0000

    Updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+// Timothy Berrill
+// CSE 02
+// Hello-World
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#   Hello World 2015   #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+} 
