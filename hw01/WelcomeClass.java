c542d14cd7c16b69d1ba7321a096550f3d0fceaa
tihm <tjb318@lehigh.edu>
2015-01-27 23:11:34 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+// Timothy Berrill
+// CSE02 Homework 1
+// Welcome Class
+
+public class WelcomeClass{
+    
+    public static void main(String[] args){
+        System.out.println("   -----------");
+        System.out.println("   | Welcome |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-T--J--B--3--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("Two years ago I went to Ecuador for a few months to do service work.");
+        System.out.println("I built a sidewalk and storage space.");
+    }
+}
